d24da61eead35c72b8bad75b57cb0050d54ce645
Aboagye=Heming <hemingkwabena@gmail.com>
2024-05-23 22:13:59 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="app">
+    <div class="header container h-100 p-5">
+      <h1 class="mb-5">Weather App</h1>
+      <div class="d-flex justfy-content-center h-100">
+        <div class="search-bar w-50 mx-2">
+          <input
+            v-model="city"
+            type="text"
+            class="input form-control"
+            placeholder="Enter a city"
+          />
+        </div>
+        <button class="btn-search btn btn-primary">Search</button>
+      </div>
+    </div>
+    <Weather></Weather>
+  </div>
+</template>
+
+<script>
+import Weather from "./components/Weather.vue";
+
+export default (await import("vue")).defineComponent({
+  name: "App",
+  components: { Weather },
+  data() {
+    return {
+      city: "",
+      showWeather: false,
+    };
+  },
+  methods: {
+    async searchWeather() {
+      this.showWeather = false;
+      await this.$nextTick();
+      this.showWeather = true;
+    },
+  },
+});
+</script>
+
+<style>
+body {
+  background-color: #121212;
+}
+.header {
+  background-color: #212730;
+  border-radius: 20px;
+  color: #fff;
+  text-align: center;
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  margin-top: 5rem;
+}
+.btn-search {
+  background-image: linear-gradient(to left, cyan, magenta);
+}
+</style>
---
+++ src/components/Weather.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="app">
+   heming
+  </div>
+</template>
+
+<script>
+export default (await import("vue")).defineComponent({
+  name: "weatherApp",
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {},
+});
+</script>
+
+<style>
+body {
+  background-color: #121212;
+}
+.header {
+  background-color: #212730;
+  border-radius: 20px;
+  color: #fff;
+  text-align: center;
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  margin-top: 5rem;
+}
+.btn-search {
+  background-image: linear-gradient(to left, cyan, magenta);
+}
+</style>
